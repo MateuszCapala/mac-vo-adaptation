d5eb5c43a8bf223260a49cac37de94d8809a797f
Mateusz Capala <mateusz.s.capala@gmail.com>
2026-03-04 13:54:24 +0000

    main folder change

--- docker_run_container.bash
+++ docker_run_container.bash
@@ -2,7 +2,7 @@ docker run --gpus all -it --rm \
   --shm-size=8g \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /home/mateusz/Desktop/magisterka/data:/data \
-  -v /home/mateusz/Desktop/magisterka/mac-vo-adaptation:/home/macvo/workspace \
+  -v /media/mateusz/D/home/magisterka/data:/data \
+  -v /media/mateusz/D/home/magisterka/mac-vo-adaptation:/home/macvo/workspace \
   --name macvo_master_thesis \
   macvo:latest
